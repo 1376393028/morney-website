a04d16e58d86bb06dc29f3a8149dffedbb89e2ac
zhazhahui <1376393028@qq.com>
2022-12-20 19:26:35 +0800

    deploy

---
+++ precache-manifest.228b5b9733ad552ed3e414156c077436.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ceada4ab4759f476548",
+    "url": "/morney-website/css/app.3db64cb3.css"
+  },
+  {
+    "revision": "8fa5691cd474c506e9e2",
+    "url": "/morney-website/css/chunk-19b7be9f.583be7a5.css"
+  },
+  {
+    "revision": "a4ef96e2682a47d538f3",
+    "url": "/morney-website/css/chunk-2f9676dc.93562440.css"
+  },
+  {
+    "revision": "1a8a32745fda92e8940f",
+    "url": "/morney-website/css/chunk-4d99409e.ff23a0d7.css"
+  },
+  {
+    "revision": "8f957c5c6b063fe498c9",
+    "url": "/morney-website/css/chunk-dc821eaa.9256f3c8.css"
+  },
+  {
+    "revision": "ccf88c07d8b7d6d3b02c",
+    "url": "/morney-website/css/chunk-vendors.30ff9def.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/morney-website/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/morney-website/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "17d9a7de8bdf3db62a56f59c504f6ba4",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "6ceada4ab4759f476548",
+    "url": "/morney-website/js/app.c5770410.js"
+  },
+  {
+    "revision": "8fa5691cd474c506e9e2",
+    "url": "/morney-website/js/chunk-19b7be9f.20ca1ae6.js"
+  },
+  {
+    "revision": "860c96ace1d75d8dd30c",
+    "url": "/morney-website/js/chunk-2d0e5e97.3695a07e.js"
+  },
+  {
+    "revision": "a4ef96e2682a47d538f3",
+    "url": "/morney-website/js/chunk-2f9676dc.9523a474.js"
+  },
+  {
+    "revision": "1a8a32745fda92e8940f",
+    "url": "/morney-website/js/chunk-4d99409e.2008e521.js"
+  },
+  {
+    "revision": "8f957c5c6b063fe498c9",
+    "url": "/morney-website/js/chunk-dc821eaa.1dc1821d.js"
+  },
+  {
+    "revision": "ccf88c07d8b7d6d3b02c",
+    "url": "/morney-website/js/chunk-vendors.ef8836ea.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "0ed88166d2587eaebf28c167db357582",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/morney-website/robots.txt"
+  }
+]);
